ba5c44a66cd4df9ed0bca12a33ae869c06d7bab5
mnhnhut0794-blip <mnhnhut0794@gmail.com>
2025-12-16 07:36:47 +0700

    DATH (CODE & WORD)

---
+++ ĐỒ ÁN TIN HỌC/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class User
+    {
+        private string _name;
+        private List<string> Friend;
+        public string Username { 
+            get { return this._name; }
+            set { this._name = value; } 
+        }
+        public List<string> Friends { 
+            get { return this.Friend; } 
+            set { this.Friend = value; }
+        }
+        public User()
+        {
+            Username = "";
+            Friends = new List<string>();
+        }
+        public User(string username)
+        {
+            Username = username;
+            Friends = new List<string>();
+        }
+    }
+}
